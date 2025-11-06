bc4fefc32c4c12474c00dd001bd09873ac6928a0
Calvin Halim <cavim1998@gmail.com>
2025-11-06 19:19:45 +0700

    adjust image

--- next.config.ts
+++ next.config.ts
@@ -16,6 +16,10 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'images.unsplash.com'
       },
+      {
+        protocol: 'https',
+        hostname: 'calvin-company-profile-test.vercel.app'
+      },
     ],
   },
 };
